69b30ef6f54d5fd81fd0a0ed242383b292185692
PF <pierre-francois.gimenez@telecom-sudparis.eu>
2015-01-05 16:10:17 +0100

    Affiche points de sortie

--- src/entryPoints/PrintPointsSortie.java
+++ src/entryPoints/PrintPointsSortie.java
@@ -1,14 +1,23 @@
 package entryPoints;
 import container.Container;
 import container.ServiceNames;
+import exceptions.ContainerException;
+import exceptions.FinMatchException;
+import exceptions.PointSortieException;
+import exceptions.ThreadException;
+import exceptions.serial.SerialManagerException;
 
 public class PrintPointsSortie {
 
 	public static void main(String[] args)
 	{
-		try {
-			(new Container()).getService(ServiceNames.A_STAR_STRATEGY);
-		} catch (Exception e) {}
+			try {
+				(new Container()).getService(ServiceNames.A_STAR_STRATEGY);
+			} catch (ContainerException | ThreadException
+					| SerialManagerException | FinMatchException
+					| PointSortieException e) {
+				e.printStackTrace();
+			}
 	}
 
 }
