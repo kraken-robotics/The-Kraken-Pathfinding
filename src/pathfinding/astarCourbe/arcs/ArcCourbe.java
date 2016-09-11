c68a4db198d75b5f42564ca7f68f5103523ecb40
PF <pierre-francois.gimenez@laposte.net>
2016-09-11 20:16:18 +0200

    …oubli d'un fichier

--- src/pathfinding/astarCourbe/arcs/ArcCourbe.java
+++ src/pathfinding/astarCourbe/arcs/ArcCourbe.java
@@ -31,7 +31,7 @@ public abstract class ArcCourbe {
 	public boolean rebrousse; // cet arc commence par un rebroussement, c'est-à-dire que la marche avant change
 	public boolean stop; // cet arc commence par un arrêt du robot
 	public int indexTrajectory;
-	public ObstacleArcCourbe obstacle;
+	public ObstacleArcCourbe obstacle = new ObstacleArcCourbe();
 	
 	public ArcCourbe(boolean rebrousse, boolean stop)
 	{
