985a83753a5cc0fbb9264f531b3665241bcdfb26
PF <pierre-francois.gimenez@irit.fr>
2016-09-28 16:29:30 +0200

    La visibilité des obstacles dépend de la hauteur des capteurs

--- src/obstacles/types/ObstaclesFixes.java
+++ src/obstacles/types/ObstaclesFixes.java
@@ -29,49 +29,62 @@ import utils.Vec2RO;
 
 public enum ObstaclesFixes {
 	// TODO visibilité à mettre à jour selon le robot / les capteurs
-	ZONE_DEPART_ENNEMI(new ObstacleRectangular(new Vec2RO(1100/2-1500,2000-350/2),1100,350), false),
-	ZONE_DEPART_A_NOUS(new ObstacleRectangular(new Vec2RO(1500-750/2,2000-350/2),750,350), false),
+	ZONE_DEPART_ENNEMI(new ObstacleRectangular(new Vec2RO(1100/2-1500,2000-350/2),1100,350), false, false),
+	ZONE_DEPART_A_NOUS(new ObstacleRectangular(new Vec2RO(1500-750/2,2000-350/2),750,350), false, false),
 
-	BAC_GAUCHE(new ObstacleRectangular(new Vec2RO(25-1500,1075),50,450), false),
-	BAC_DROIT(new ObstacleRectangular(new Vec2RO(1500-25,1075),50,450), false),
-
-	CENTRE_VILLAGE(new ObstacleCircular(new Vec2RO(0,0),200), false),
-
-	SUPPORT_MODULE_GAUCHE(new ObstacleRectangular(new Vec2RO(0,500).rotateNewVector(-Math.PI/4, new Vec2RO(0,0)),140,600, -Math.PI/4), false),
-	SUPPORT_MODULE_MILIEU(new ObstacleRectangular(new Vec2RO(0,500),140,600), false),
-	SUPPORT_MODULE_DROITE(new ObstacleRectangular(new Vec2RO(0,500).rotateNewVector(Math.PI/4, new Vec2RO(0,0)),140,600, Math.PI/4), false),
+	// TODO baguettes
 	
-	PETIT_CRATERE_HAUT_GAUCHE(new ObstacleCircular(new Vec2RO(650-1500,2000-530),90), false),
-	PETIT_CRATERE_HAUT_DROITE(new ObstacleCircular(new Vec2RO(1500-650,2000-530),90), false),
+	BAC_GAUCHE(new ObstacleRectangular(new Vec2RO(25-1500,1075),50,450), true, false),
+	BAC_DROIT(new ObstacleRectangular(new Vec2RO(1500-25,1075),50,450), true, false),
 
-	PETIT_CRATERE_BAS_GAUCHE(new ObstacleCircular(new Vec2RO(1100-1500,2000-1870),90), false),
-	PETIT_CRATERE_BAS_DROITE(new ObstacleCircular(new Vec2RO(1500-1100,2000-1870),90), false),
+	CENTRE_VILLAGE(new ObstacleCircular(new Vec2RO(0,0),200), true, false),
 
-	GROS_CRATERE_GAUCHE(new ObstacleCircular(new Vec2RO(-1500,0),575), false),
-	GROS_CRATERE_DROITE(new ObstacleCircular(new Vec2RO(1500,0),575), false),
+	SUPPORT_MODULE_GAUCHE(new ObstacleRectangular(new Vec2RO(0,500).rotateNewVector(-Math.PI/4, new Vec2RO(0,0)),140,600, -Math.PI/4), true, false),
+	SUPPORT_MODULE_MILIEU(new ObstacleRectangular(new Vec2RO(0,500),140,600), true, false),
+	SUPPORT_MODULE_DROITE(new ObstacleRectangular(new Vec2RO(0,500).rotateNewVector(Math.PI/4, new Vec2RO(0,0)),140,600, Math.PI/4), true, false),
+	
+	PETIT_CRATERE_HAUT_GAUCHE(new ObstacleCircular(new Vec2RO(650-1500,2000-530),90), false, false),
+	PETIT_CRATERE_HAUT_DROITE(new ObstacleCircular(new Vec2RO(1500-650,2000-530),90), false, false),
 
-	FUSEE_GAUCHE(new ObstacleCircular(new Vec2RO(-350,1960),40), true),	
-	FUSEE_MILIEU(new ObstacleCircular(new Vec2RO(0,1960),40), true),	
-	FUSEE_DROITE(new ObstacleCircular(new Vec2RO(350,1960),40), true),	
+	PETIT_CRATERE_BAS_GAUCHE(new ObstacleCircular(new Vec2RO(1100-1500,2000-1870),90), false, false),
+	PETIT_CRATERE_BAS_DROITE(new ObstacleCircular(new Vec2RO(1500-1100,2000-1870),90), false, false),
+
+	GROS_CRATERE_GAUCHE(new ObstacleCircular(new Vec2RO(-1500,0),575), false, false),
+	GROS_CRATERE_DROITE(new ObstacleCircular(new Vec2RO(1500,0),575), false, false),
+
+	FUSEE_GAUCHE(new ObstacleCircular(new Vec2RO(-350,1960),40), true, true),	
+	FUSEE_MILIEU(new ObstacleCircular(new Vec2RO(0,1960),40), true, true),	
+	FUSEE_DROITE(new ObstacleCircular(new Vec2RO(350,1960),40), true, true),	
 	
 	// bords
-    BORD_BAS(new ObstacleRectangular(new Vec2RO(0,0),3000,5,Layer.BACKGROUND), false),
-    BORD_GAUCHE(new ObstacleRectangular(new Vec2RO(-1500,1000),5,2000,Layer.BACKGROUND), false),
-    BORD_DROITE(new ObstacleRectangular(new Vec2RO(1500,1000),5,2000,Layer.BACKGROUND), false),
-    BORD_HAUT(new ObstacleRectangular(new Vec2RO(0,2000),3000,5,Layer.BACKGROUND), false);
+    BORD_BAS(new ObstacleRectangular(new Vec2RO(0,0),3000,5,Layer.BACKGROUND), true, false),
+    BORD_GAUCHE(new ObstacleRectangular(new Vec2RO(-1500,1000),5,2000,Layer.BACKGROUND), true, false),
+    BORD_DROITE(new ObstacleRectangular(new Vec2RO(1500,1000),5,2000,Layer.BACKGROUND), true, false),
+    BORD_HAUT(new ObstacleRectangular(new Vec2RO(0,2000),3000,5,Layer.BACKGROUND), true, false);
 
     private final Obstacle obstacle;
-    public final boolean visible;
+    private final boolean[] visible = new boolean[2];
     
-    private ObstaclesFixes(Obstacle obstacle, boolean visible)
+    private ObstaclesFixes(Obstacle obstacle, boolean visibleBas, boolean visibleHaut)
     {
     	this.obstacle = obstacle;
-    	this.visible = visible;
+    	visible[0] = visibleBas;
+    	visible[1] = visibleHaut;
     }
 
     public Obstacle getObstacle()
     {
     	return obstacle;
     }
+
+    /**
+     * Cet obstacle est-il visible pour un capteur surélevé ou non ?
+     * @param sureleve
+     * @return
+     */
+	public boolean isVisible(boolean sureleve)
+	{
+		return visible[sureleve ? 1 : 0];
+	}
 
 }
