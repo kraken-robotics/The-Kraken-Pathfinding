89b07a16a9701ed3d937e99c817253c05571d49d
PF <techthetroll@gmail.com>
2017-02-11 14:41:41 +0100

    Update entête de fichiers

--- src/pathfinding/dstarlite/Cle.java
+++ src/pathfinding/dstarlite/Cle.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2016 Pierre-François Gimenez
+Copyright (C) 2013-2017 Pierre-François Gimenez
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
