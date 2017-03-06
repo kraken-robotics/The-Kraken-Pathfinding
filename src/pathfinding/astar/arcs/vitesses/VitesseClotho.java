70b56198cdf256cbda0f4a06926da739fe1ffe99
PF <pierre-francois.gimenez@laposte.net>
2017-03-06 02:35:20 +0100

    Limitation de la dérivée de la courbure

--- src/pathfinding/astar/arcs/vitesses/VitesseClotho.java
+++ src/pathfinding/astar/arcs/vitesses/VitesseClotho.java
@@ -33,19 +33,19 @@ public enum VitesseClotho implements VitesseCourbure
 	GAUCHE_1(4),
 	GAUCHE_2(9),
 	GAUCHE_3(16),
-	GAUCHE_4(25),
-	GAUCHE_5(36),
+//	GAUCHE_4(25),
+//	GAUCHE_5(36),
 	COURBURE_IDENTIQUE(0),
 	DROITE_0(-1),
 	DROITE_1(-4),
 	DROITE_2(-9),
 	DROITE_3(-16),
-	DROITE_4(-25),
-	DROITE_5(-36),
+//	DROITE_4(-25),
+//	DROITE_5(-36),
 	
-	GAUCHE_3_REBROUSSE(16),
-	COURBURE_IDENTIQUE_REBROUSSE(0),
-	DROITE_3_REBROUSSE(-16);
+//	GAUCHE_3_REBROUSSE(16),
+//	DROITE_3_REBROUSSE(-16),
+	COURBURE_IDENTIQUE_REBROUSSE(0);
 
 	public final int vitesse; // vitesse en (1/m)/m = 1/m^2
 	public final int squaredRootVitesse; // sqrt(abs(vitesse))
