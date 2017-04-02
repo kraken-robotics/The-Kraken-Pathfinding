f9868cfd717a49fcdbc241d8cafa166040fa7fe5
PF <techthetroll@gmail.com>
2017-04-02 22:29:49 +0200

    Protocole couleur

--- src/tests/lowlevel/JUnit_Serie.java
+++ src/tests/lowlevel/JUnit_Serie.java
@@ -21,7 +21,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import serie.BufferOutgoingOrder;
-import serie.SerialProtocol.InOrder;
 import serie.SerialProtocol.State;
 import serie.Ticket;
 import tests.JUnit_Test;
@@ -77,10 +76,8 @@ public class JUnit_Serie extends JUnit_Test {
 		State etat;
 		do {
 			Ticket t = data.demandeCouleur();
-			InOrder o = t.attendStatus();
-			log.debug(o);
-			etat = o.etat;
-			Thread.sleep(500);			
+			etat = t.attendStatus().etat;
+			Thread.sleep(500);
 		} while(etat != State.OK);
 	}
 	
