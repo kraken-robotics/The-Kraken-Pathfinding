63ffbed11701256b9b1f173093e1ea5acf35c1a8
PF <techthetroll@gmail.com>
2017-06-14 19:04:41 +0200

    Test correction

--- tests/src/JUnit_AStarCourbe.java
+++ tests/src/JUnit_AStarCourbe.java
@@ -7,7 +7,7 @@ import java.util.Random;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import graphic.PrintBufferInterface;
+import graphic.AbstractPrintBuffer;
 import kraken.ConfigInfoKraken;
 import kraken.exceptions.PathfindingException;
 import kraken.obstacles.types.ObstacleCircular;
@@ -41,7 +41,7 @@ public class JUnit_AStarCourbe extends JUnit_Test
 	private AStarCourbe astar;
 	private ClothoidesComputer clotho;
 	protected BezierComputer bezier;
-	private PrintBufferInterface buffer;
+	private AbstractPrintBuffer buffer;
 	private boolean graphicTrajectory;
 	private DefaultCheminPathfinding fakeChemin;
 	private GridSpace gridspace;
@@ -56,7 +56,7 @@ public class JUnit_AStarCourbe extends JUnit_Test
 	{
 		super.setUp();
 		clotho = injector.getService(ClothoidesComputer.class);
-		buffer = injector.getService(PrintBufferInterface.class);
+		buffer = injector.getService(AbstractPrintBuffer.class);
 		astar = injector.getService(AStarCourbe.class);
 		// dstarlite = injector.getService(DStarLite.class);
 		gridspace = injector.getService(GridSpace.class);
