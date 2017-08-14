376b91dc5dfc45b2070bfd036087f7aeb3ebb5f4
PF <pierre-francois.gimenez@laposte.net>
2017-08-14 19:29:06 +0200

    Bugfix

--- core/compile-lib.sh
+++ core/compile-lib.sh
@@ -12,14 +12,12 @@ cd ../..
 
 git clone https://github.com/PFGimenez/graphic-toolbox.git
 cp dependency-injector/injector.jar graphic-toolbox/core/lib
-cp config/config.jar graphic-toolbox/core/lib
+cp config/core/config.jar graphic-toolbox/core/lib
 cd graphic-toolbox/core
 ant
 cd ../..
 
 git clone https://github.com/PFGimenez/The-Kraken-Pathfinding.git
-cp dependency-injector/injector.jar The-Kraken-Pathfinding/core/lib
-cp graphic-toolbox/core/graphic.jar The-Kraken-Pathfinding/core/lib
-cp config/core/config.jar The-Kraken-Pathfinding/core/lib
-cd The-Kraken-Pathfinding/core/
-ant
+cp dependency-injector/injector.jar lib
+cp graphic-toolbox/core/graphic.jar lib
+cp config/core/config.jar lib
